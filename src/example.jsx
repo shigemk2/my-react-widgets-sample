38b016cbeac3ef11d3192f8db341c4e2c65ac17f
shigemk2 <i.am.shige@gmail.com>
2016-04-06 02:32:38 +0900

    Add sample

--- src/example.jsx
+++ src/example.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Name = React.createClass({
-    render: function() {
-        return (
-            <span>{this.props.name}</span>
-        );
-    }
-});
+import DropdownList from 'react-widgets/lib/DropdownList';
+
 const HelloWorld = React.createClass({
     render: function() {
         return (
             <div>
-                <h1>Hello, world!</h1>
-                <Name name="Test" />
+                <DropdownList/>
             </div>
         );
     }
