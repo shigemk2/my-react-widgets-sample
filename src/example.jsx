4c4663ef3a2445a4d46cbeb7ca95a64b636fa17d
shigemk2 <i.am.shige@gmail.com>
2016-04-06 02:35:07 +0900

    ドロップダウンリスト追加

--- src/example.jsx
+++ src/example.jsx
@@ -5,9 +5,10 @@ import DropdownList from 'react-widgets/lib/DropdownList';
 
 const HelloWorld = React.createClass({
     render: function() {
+        var colors = ['orange', 'red', 'blue', 'purple'];
         return (
             <div>
-                <DropdownList/>
+                <DropdownList defaultValue={"orange"} data={colors}/>
             </div>
         );
     }
